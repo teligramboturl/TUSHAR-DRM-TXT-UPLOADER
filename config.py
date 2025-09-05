34440ed8127eae67ac0260e63d87f21237eccef1
teligramboturl <teligramboturl@gmail.com>
2025-09-06 01:16:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "24473318") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace With your api hash
+API_ID = os.environ.get("API_ID", "23673651") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b") #Replace With your api hash
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace With your bot token
