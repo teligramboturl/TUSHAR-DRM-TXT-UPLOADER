128374f0272a5c87f1c6c0bcdff2a0d3ebdd577e
teligramboturl <teligramboturl@gmail.com>
2025-09-06 10:08:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,12 +48,12 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 6677821706 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311]
+SUDO_USERS = [6677821706]
 
-AUTH_CHANNEL = -1002595188554
+AUTH_CHANNEL = -1002685378156
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
